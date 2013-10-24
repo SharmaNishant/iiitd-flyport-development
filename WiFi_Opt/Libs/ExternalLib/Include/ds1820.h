523baf06dc033fa8a7b9e7ae53710e084cb87f88
nishant11070@iiitd.ac.in <nishant11070@iiitd.ac.in@64539008-7100-2c19-ac69-a3c022202f5c>
2013-10-24 00:56:56 +0000

    changed temprature sensor type for WiFi_Opt

--- WiFi_Opt/Libs/ExternalLib/Include/ds1820.h
+++ WiFi_Opt/Libs/ExternalLib/Include/ds1820.h
@@ -4,7 +4,7 @@ void DS1820Init();
 
 #define DQ_PORT I5					// defines the port used for the data line of the DS1820 sensor
 //#define DS1820_9bit				// defines 9bit mode documented in DS1820 data-sheet
-#define DS1820_higher_resolution	// defines higher resolution mode describes in data-sheet
+//#define DS1820_higher_resolution	// defines higher resolution mode describes in data-sheet
 //#define DS18B20_9bit_resolution	// defines lower resolution mode  for DS18B20  as described in data-sheet
-//#define DS18B20_higher_resolution	// defines higher resolution mode for DS18B20  as described in data-sheet
+#define DS18B20_higher_resolution	// defines higher resolution mode for DS18B20  as described in data-sheet
 
